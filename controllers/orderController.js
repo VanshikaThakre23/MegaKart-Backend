b0e8cd7c00dc03867c091c4f186624bc0773cab7
Vanshika <vanshikathakre4@gmail.com>
2026-04-15 16:00:04 +0530

    updated order controller.js

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,37 +1,38 @@
 const orderModel = require("../models/orderModel.js");
 
-exports.placeOrder = async(req,res)=>{
+exports.placeOrder = async (req, res) => {
     try {
-        const {items , addresses , totalAmount ,paymentMethod} = req.body;
-        const newOrder= new orderModel({
-        user : req.user._id,
-        items,
-        addresses,
-        totalAmount,
-        paymentMethod,
+        const { items, addresses, totalAmount, paymentMethod } = req.body;
+        const newOrder = new orderModel({
+            user: req.user._id,
+            items,
+            addresses,
+            totalAmount,
+            paymentMethod,
         })
 
         await newOrder.save();
-        res.status(200).json({success:true,data:newOrder});
-        
+        res.status(200).json({ success: true, data: newOrder });
+
     } catch (error) {
-        res.json({message:"error" ,error:error.message})
+        res.json({ message: "error", error: error.message })
     }
 }
 
-exports.getMyOrder = async(req,res)=>{
+exports.getMyOrder = async (req, res) => {
     try {
-        const order = await orderModel.find((user.req._id).populate("items._id"));
-        res.json({success:true,data:order});
+
+        const order = await orderModel.find({ user: req.user._id }).populate("items.product").sort({ createdAt: -1 });;
+        res.json({ success: true, data: order });
     } catch (error) {
         res.json(error);
     }
 }
 
-exports.seeAllOrder = async(req,res)=>{
+exports.seeAllOrder = async (req, res) => {
     try {
         const order = await orderModel.find().populate("user").populate("items.product").populate("addresses");
-        res.json({success:true,data:order});
+        res.json({ success: true, data: order });
     } catch (error) {
         res.json(error);
     }
